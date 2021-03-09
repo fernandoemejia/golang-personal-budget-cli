54bc2beefbd6a0c3ef9125eb6f58b9ce2386b0f7
Fernando Mejia <fernandoemejia@yahoo.com>
2021-03-09 15:01:27 -0500

    Complete Module3

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -57,7 +57,7 @@ func (b *Budget) AddItem(description string, price float32) error {
 		return errDoesNotFitBudget
 	}
 	newItem := Item{Description: description, Price: price}
-	append(b.Items, newItem)
+	b.Items = append(b.Items, newItem)
 	return nil
 }
 
@@ -65,7 +65,7 @@ func (b *Budget) AddItem(description string, price float32) error {
 func (b *Budget) RemoveItem(description string) {
 	for i := range b.Items {
 		if b.Items[i].Description == description {
-			append(b.Items[:i], b.Items[i+1:]...)
+			b.Items = append(b.Items[:i], b.Items[i+1:]...)
 			break
 		}
 	}
@@ -74,13 +74,24 @@ func (b *Budget) RemoveItem(description string) {
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
+	newBudget = &Budget{Max: max}
+	if len(report) >= 12 {
 
+		return nil, errReportIsFull
+	}
+	if _, hasEntry := report[month]; hasEntry {
+
+		return nil, errDuplicateEntry
+	}
+	report[month] = newBudget
 	return newBudget, nil
 }
 
 // GetBudget returns budget for given month
 func GetBudget(month time.Month) *Budget {
-
+	if budget, ok := report[month]; ok {
+		return budget
+	}
 	return nil
 }
 
